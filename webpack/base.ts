20923b7e928a00afa9e7b62ac3d6b99587849774
MrJackphil <geimerus@gmail.com>
2019-06-22 11:38:37 +0300

    phaser-ts-default-setup

--- webpack/base.ts
+++ webpack/base.ts
@@ -4,10 +4,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
+  entry: './src/index.ts',
   mode: "development",
   devtool: "eval-source-map",
   module: {
     rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+			{ loader: "ts-loader" },
+			{ loader: "babel-loader" },
+		]
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
